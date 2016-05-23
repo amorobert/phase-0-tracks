7861007d52fe6430538d1f9b81fba1a931ad57fc
Robert Washington <amorobert@gmail.com>
2016-05-23 10:30:26 -0500

    completed quetion 4

--- ruby/fantasy_dance_league/fantasy_dancers.rb
+++ ruby/fantasy_dance_league/fantasy_dancers.rb
@@ -1,10 +1,11 @@
 class Dancer
-attr_reader :name
+attr_reader :name, :card
 attr_accessor :age
 
 def initialize(name, age)
   @name = name
   @age = age
+  @card = []
 end
 
 def pirouette
@@ -13,5 +14,19 @@ end
 
 def bow
   p "*bows*"
+end
+
+def queue_dance_with(dancer)
+  @card << dancer
+end
+
+def begin_next_dance
+  output = "Now dancing with #{@card[0]}."
+  @card.shift
+  output
+end
+#####Makes dancer leap
+def leap
+  p "*leaps*"
 end
 end
