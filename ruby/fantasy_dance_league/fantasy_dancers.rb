9616cc607c0d5ad74a2af4c15cd17655e585f051
Robert Washington <amorobert@gmail.com>
2016-05-23 10:04:30 -0500

    completed question 2

--- ruby/fantasy_dance_league/fantasy_dancers.rb
+++ ruby/fantasy_dance_league/fantasy_dancers.rb
@@ -1,8 +1,17 @@
 class Dancer
 attr_reader :name
 attr_accessor :age
+
 def initialize(name, age)
   @name = name
   @age = age
+end
+
+def pirouette
+  p "*twirls*"
+end
+
+def bow
+  p "*bows*"
 end
 end
