606327c18c1658854ee71413a629160b9036e4cb
Robert Washington <amorobert@gmail.com>
2016-05-24 04:59:07 -0500

    fixed directory name

---
+++ ruby/gps2_2/grocery_list.rb
@@ -0,0 +1,42 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # Split the string into an hash of idividual items and their quantity
+  # set default quantity
+  # print the list to the console using the print method
+# output: hash
+
+# Method to add an item to a list
+# input: item and optional quantity (example: "apples 6")
+# steps:
+  #Set the variable 'list' equal to the hash output of the print method
+  #Store input item and quantity as key, value pair in 'list'
+  #print the 'list' to the console using the print method
+# output:hash
+
+# Method to remove an item from the list
+# input: item (example: "cereal")
+# steps:
+  #Set the variable 'list' equal to the hash output of the print method
+  #Store input item and quantity as key, value pair in 'list'
+  #identify the index of item to remove
+  #delete item from the 'list'
+  #print the 'list' to the console using the print method
+# output:hash
+
+# Method to update the quantity of an item
+# input:item and quantity (example: "pizza 2")
+# steps:
+  #Set the variable 'list' equal to the hash output of the print method
+  #identify the index of item to to update
+  #set the quantity of the item to the new quantity
+  # print the 'list' to the console using the print method
+# output:
+
+# Method to print a list and make it look pretty
+# input: none
+# steps:
+  #Print each key value pair using the puts method
+# output:string
+
+def create_list(items)
---
+++ ruby/gps2_2/grocery_list_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'grocery_list'
+
+describe Electronic_grocery_list do
+  it "creates a new list" do
+  end
+
+  it "adds an item to the list" do
+  end
+
+  it "removes an item from the list" do
+  end
+
+  it "updates the quantity of an item on the list" do
+  end
+
+  it "prints out the list" do
+  end
+end
