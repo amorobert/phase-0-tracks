279e4299a8e19f2fb49146f51f31e68fa263a441
Robert Washington <amorobert@gmail.com>
2016-05-26 19:10:57 -0500

    Completed challenge 7.1

--- js/explore.js
+++ js/explore.js
@@ -2,3 +2,16 @@
 // -Create a loop that stops once its looped as many times as the length of the inputString
 //  -Access each leter of inputString using its index
 //  -Output the last letter of the string, outputting the preceding letter thereafter
+
+
+function reverse(inputString) {
+  var outputString = '';
+
+  for (var i = inputString.length - 1; i >= 0; i--) {
+    outputString += inputString[i];
+  }
+
+  return outputString;
+}
+
+reverse("hello");
