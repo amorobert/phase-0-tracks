dfacc9ba641096376fdf5fe6390e79e0cd6e76ef
Robert Washington <amorobert@gmail.com>
2016-05-26 21:14:11 -0500

    completed 7.2 release 0

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colors = ["blue", "green", "yellow", "brown"];
+var names = ["ed", "shirley", "bella", "rodrick"];
+
+function newHorse(name, color) {
+  names.push(name);
+  colors.push(color);
+}
