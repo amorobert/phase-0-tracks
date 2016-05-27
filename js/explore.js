5ad066dd5f1a4100716e3759c161774438eb7ba8
Robert Washington <amorobert@gmail.com>
2016-05-26 19:10:04 -0500

    wrote pseudocode for release 4

---
+++ js/explore.js
@@ -0,0 +1,4 @@
+// -Store the user input string in the variable inputString
+// -Create a loop that stops once its looped as many times as the length of the inputString
+//  -Access each leter of inputString using its index
+//  -Output the last letter of the string, outputting the preceding letter thereafter
